34cdad54f2bb733c1fa7df55bf7a068a9abb57df
JWaldenback <joel.waldenback@outlook.com>
2022-09-20 16:50:28 +0200

    Move functionality of toggling microphone from talon_hud to knausj_talon

--- content/my_customizations/my_customizations.py
+++ content/my_customizations/my_customizations.py
@@ -14,11 +14,3 @@ ctx=Context()
 class UserActions:
     def poll_microphone():
         """sdf"""
-        current_microphone = actions.sound.active_microphone()
-        if current_microphone == "None":
-            #https://github.com/chaosparrot/talon_hud/blob/master/CUSTOMIZATION.md#log-messages
-            actions.user.hud_add_log('success', 'Microphone enabled')
-        else:
-            actions.user.hud_add_log('error', 'Microphone disabled')
-        
-        actions.user.hud_toggle_microphone()
